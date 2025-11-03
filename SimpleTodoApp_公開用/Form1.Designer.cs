3eb2081cecaf24a73b1f15d4597a6fade1bedce1
aosuzu1003 <rkrn1003@gmail.com>
2025-11-03 12:15:03 +0900

    Add files via upload
    git練習
    初回コミット

---
+++ SimpleTodoApp_公開用/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SimpleTodoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTask = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.listBoxTasks = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxTask
+            // 
+            this.textBoxTask.Location = new System.Drawing.Point(30, 40);
+            this.textBoxTask.Name = "textBoxTask";
+            this.textBoxTask.Size = new System.Drawing.Size(320, 25);
+            this.textBoxTask.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(30, 100);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(150, 50);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "追加";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(200, 100);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(150, 50);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "削除";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // listBoxTasks
+            // 
+            this.listBoxTasks.FormattingEnabled = true;
+            this.listBoxTasks.ItemHeight = 18;
+            this.listBoxTasks.Location = new System.Drawing.Point(30, 180);
+            this.listBoxTasks.Name = "listBoxTasks";
+            this.listBoxTasks.Size = new System.Drawing.Size(320, 184);
+            this.listBoxTasks.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 444);
+            this.Controls.Add(this.listBoxTasks);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxTask);
+            this.Name = "Form1";
+            this.Text = "SimpleTodoApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTask;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ListBox listBoxTasks;
+    }
+}
+
---
+++ SimpleTodoApp_公開用/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+using System.IO;
+
+namespace SimpleTodoApp
+{
+    public partial class Form1 : Form
+    {
+        //タスクを格納するリスト
+        List<string> tasks = new List<string>();
+        //タスクを管理するテキストファイル
+        string filePath = "tasks.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTasks();
+        }
+
+        //テキストファイルからタスクを読み込む
+        private void LoadTasks()
+        {
+            //テキストファイルが存在したら
+            if (File.Exists(filePath))
+            {
+                //テキストファイルの内容をリストに格納
+                tasks.AddRange(File.ReadAllLines(filePath));
+                //タスクリストの内容をリストボックスに追加
+                listBoxTasks.Items.AddRange(tasks.ToArray());
+            }
+        }
+
+        //追加ボタン押下時
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            //テキストボックスが空以外なら
+            if (!string.IsNullOrWhiteSpace(textBoxTask.Text))
+            {
+                //テキストボックスの内容をリストに格納
+                tasks.Add(textBoxTask.Text);
+                //タスクボックスの内容をリストボックスに追加
+                listBoxTasks.Items.Add(textBoxTask.Text);
+                //テキストボックスをクリアする
+                textBoxTask.Clear();
+                //ファイルを保存する
+                File.WriteAllLines(filePath, tasks);
+            }
+        }
+
+        //削除ボタン押下時
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            //リストボックス内のアイテムが選択されていたら
+            if (listBoxTasks.SelectedItem != null)
+            {
+                //リストから選択中のアイテムを削除
+                tasks.Remove(listBoxTasks.SelectedItem.ToString());
+                //リストボックスから選択中のアイテムを削除
+                listBoxTasks.Items.Remove(listBoxTasks.SelectedItem);
+                //ファイルを保存
+                File.WriteAllLines(filePath, tasks);
+            }
+        }
+    }
+}
